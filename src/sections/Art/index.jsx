9c629b2e28a55e5da3fa4535e327a1de5b2ad2e5
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-27 14:33:01 +0200

    Refactor Art section and add ArtList component

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
+import Artlist from "./components/ArtList";
+import { fetchData } from "../../helpers/FetchData";
+
 function ArtsSection() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    const fetchArtworks = async () => {
+      const data = await fetchData("https://boolean-uk-api-server.fly.dev/art");
+      setArtworks(data);
+    };
+
+    fetchArtworks();
+  }, []);
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <Artlist artworks={artworks} />
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
