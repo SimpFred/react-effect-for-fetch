906fa95bb0e0cd81ee5873da2117f5bbf557e520
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-27 14:33:37 +0200

    Refactor AdviceSection component and add functionality to fetch and save advice slips

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../../helpers/FetchData";
+import AdviceSlip from "./components/AdviceSlip";
+import FavouriteSlipsList from "./components/FavouriteSlipsList";
+
 function AdviceSection() {
+  const [advice, setAdvice] = useState("");
+  const [favourites, setFavourites] = useState([]);
+
+  const fetchAdvice = async () => {
+    const data = await fetchData("https://api.adviceslip.com/advice");
+    setAdvice(data.slip.advice);
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const saveToFavourites = () => {
+    if (advice && !favourites.includes(advice)) {
+      setFavourites([...favourites, advice]);
+    }
+  };
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <AdviceSlip
+        advice={advice}
+        fetchAdvice={fetchAdvice}
+        saveToFavourites={saveToFavourites}
+      />
+      <FavouriteSlipsList favouriteSlips={favourites} />
     </section>
-  )
+  );
 }
 
-export default AdviceSection
+export default AdviceSection;
